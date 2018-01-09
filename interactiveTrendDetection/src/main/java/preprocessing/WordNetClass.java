d19a51af42c95566b3d64bd32bdd7a86cb202129
Noushin <Noushin@147.172.84.90>
2018-01-09 20:12:00 +0100

    depth1 calculated, LCS and depth2 possibilities are added.

--- interactiveTrendDetection/src/main/java/preprocessing/WordNetClass.java
+++ interactiveTrendDetection/src/main/java/preprocessing/WordNetClass.java
@@ -21,9 +21,20 @@ import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 import edu.cmu.lti.ws4j.util.DepthFinder;
 import edu.cmu.lti.ws4j.util.PathFinder;
 import edu.cmu.lti.ws4j.util.DepthFinder.Depth;
+import edu.cmu.lti.jawjaw.pobj.Lang;
 import edu.cmu.lti.jawjaw.pobj.POS;
+import edu.cmu.lti.jawjaw.pobj.Sense;
+import edu.cmu.lti.jawjaw.pobj.Synlink;
+import edu.cmu.lti.jawjaw.pobj.Synset;
+import edu.cmu.lti.jawjaw.pobj.SynsetDef;
+import edu.cmu.lti.jawjaw.pobj.Word;
 import edu.cmu.lti.jawjaw.util.WordNetUtil;
 import edu.cmu.lti.jawjaw.JAWJAW;
+import edu.cmu.lti.jawjaw.db.SenseDAO;
+import edu.cmu.lti.jawjaw.db.SynlinkDAO;
+import edu.cmu.lti.jawjaw.db.SynsetDAO;
+import edu.cmu.lti.jawjaw.db.SynsetDefDAO;
+import edu.cmu.lti.jawjaw.db.WordDAO;
 
 
 public class WordNetClass {
@@ -39,31 +50,90 @@ public class WordNetClass {
 	public static void main(String[] args) {
 		
 		
-		String word1="material";
+		String word1="material";//"use";
 		DepthFinder depthfinder=new DepthFinder(db);
 		PathFinder pathfinder=new PathFinder(db);
-		int depth1;
+		
 		POS pos = POS.valueOf("n");
-		List<edu.cmu.lti.jawjaw.pobj.Synset> synsets = WordNetUtil.wordToSynsets(word1, pos);
-		List<Concept> synsetStrings = new ArrayList<Concept>(synsets.size());
+		//List<Synset> synsets = WordNetUtil.wordToSynsets(word1, pos);
+		//List<Concept> synsetStrings = new ArrayList<Concept>(synsets.size());
 		double criterionSimilarityDegree=0.8;
 		
-		for ( edu.cmu.lti.jawjaw.pobj.Synset synset : synsets ) {
+		List<Word> words = WordDAO.findWordsByLemmaAndPos(word1, pos);
+		List<Sense> senses = SenseDAO.findSensesByWordid( words.get(0).getWordid() );
+		List<Concept> synsetStrings = new ArrayList<Concept>(senses.size());	
+		//String synsetId = senses.get(0).getSynset();
+		//Synset synset1 = SynsetDAO.findSynsetBySynset( synsetId );
+		//SynsetDef synsetDef = SynsetDefDAO.findSynsetDefBySynsetAndLang(synsetId, Lang.eng);
+		//List<Synlink> synlinks = SynlinkDAO.findSynlinksBySynset( synsetId );
+	
+		// Showing the result
+
+		System.out.println( words.get(0) );
+
+		System.out.println( senses.get(0) );
+
+		//System.out.println( synset1 );
+
+		//System.out.println( synsetDef );
+
+		//System.out.println( synlinks.get(0) );
+		
+		/*for (Synset synset : synsets ) {
+			System.out.println(synset);
 			synsetStrings.add(new Concept(synset.getSynset(), POS.valueOf(pos.toString())));
 			
+		}*/
+		String synsetId;
+		Synset synset1 ;
+		for(Sense sense: senses){
+			synsetId = sense.getSynset();
+			synset1 = SynsetDAO.findSynsetBySynset( synsetId );
+			System.out.println( synset1 );
+			synsetStrings.add(new Concept(synsetId, POS.valueOf(pos.toString())));
 		}
 		
-			//computing the depth of the word indicating the desired criterion (d1)
-		    Concept synset=synsetStrings.get(0);
-		   
-		    depth1 = depthfinder.getShortestDepth( synset);
-		    System.out.println(depth1);
+			//The depth of the word indicating the desired criterion (d1)
+			int depth1;
+		    Concept con=synsetStrings.get(0);
+		    depth1 = depthfinder.getShortestDepth( con);
+		    System.out.println("depth1:"+ depth1);
+		    
+		    //LCS possiblities
+		    List<Integer> LCSList=new ArrayList<Integer>();
+		    int lowerBound_LCS= (int) Math.ceil((double)2*depth1/3);
+		    int upperBound_LCS=depth1+1;
+		    for(int i=lowerBound_LCS; i<upperBound_LCS;i++){
+		    	LCSList.add(i);		   
+		    }
+		    
+		    //depth2 (d2) possiblities
+		    List<Integer> d2List=new ArrayList<Integer>();
+		    int upperBound_d2;
+		    int lowerBound_d2;
+		    
+		    for(int LCS:LCSList){
+		    	lowerBound_d2=LCS;
+		    	upperBound_d2=(int) Math.floor((double) 2.5*LCS - depth1) +1;
+		    	for(int i=lowerBound_d2; i<upperBound_d2;i++){
+			    	d2List.add(i);		   
+			    }
+		    	System.out.println("For LCS= "+LCS+" d2 possiblities are: "+d2List);
+		    	d2List.clear();
+		    	}
+		    
 		   
 			Set<String> hyponyms = JAWJAW.findHyponyms(word1, pos);
 			
 			
 			Set<String> history = new HashSet<String>();
-			List<List<String>> paths = pathfinder.getHypernymTrees(word1, history);
+			List<List<String>> hyperTree1 = pathfinder.getHypernymTrees(senses.get(0).getSynset(), history);
+			for(List<String> onePath: hyperTree1){
+				
+					System.out.println(onePath);	
+				
+				
+			}
 			
 			
 			Set<String> nextGeneration = new HashSet<String>();
